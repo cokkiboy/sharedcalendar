54c4772eebabfc99a362a9fc4abe4fd9e3ee799f
naseokhoo <nahoo1202@naver.com>
2023-12-02 07:45:06 +0900

    로그아웃 구현

--- app/src/main/java/com/app/sharedcalendar/MainActivity.kt
+++ app/src/main/java/com/app/sharedcalendar/MainActivity.kt
@@ -1,12 +1,14 @@
+package com.app.sharedcalendar
 
- package com.app.sharedcalendar
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.CalendarView
 import android.widget.TextView
 import android.widget.Toast
-
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -14,6 +16,7 @@ import com.app.sharedcalendar.Schedule.ScheduleAdapter
 import com.app.sharedcalendar.Schedule.ScheduleInputActivity
 import com.app.sharedcalendar.Schedule.ScheduleItem
 import com.app.sharedcalendar.Schedule.ScheduleListActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 
@@ -30,7 +33,7 @@ class MainActivity : AppCompatActivity() {
     private var selectedDate: String = ""
     private lateinit var recyclerView: RecyclerView
     private lateinit var scheduleAdapter: ScheduleAdapter
-
+    private lateinit var bottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,6 +44,7 @@ class MainActivity : AppCompatActivity() {
         deleteBtn = findViewById(R.id.deleteBtn)
         diaryContent = findViewById(R.id.diaryContent)
         calendarView = findViewById(R.id.calendarView)
+        bottomNavigationView = findViewById(R.id.navigationView)
 
         userID = intent.getStringExtra("userID") ?: ""
         database = FirebaseDatabase.getInstance().reference
@@ -75,77 +79,79 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
             loadDiary(selectedDate)
         }
+
         recyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
-        scheduleAdapter = ScheduleAdapter(ArrayList()) // 초기에는 빈 목록으로 시작
+        scheduleAdapter = ScheduleAdapter(ArrayList())
         recyclerView.adapter = scheduleAdapter
 
-        // 일정 목록을 불러오는 함수 호출
-
         loadDiary(selectedDate)
+
+        // BottomNavigationView 아이템 선택 리스너 설정
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.calenderFragment -> {
+                    showLogoutConfirmationDialog()
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.homeFragment -> {
+                    // Home 아이템이 선택되었을 때 추가 작업 또는 화면 전환 코드 작성
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.myPageFragment -> {
+                    // 검색 아이템이 선택되었을 때 추가 작업 또는 화면 전환 코드 작성
+                    return@setOnNavigationItemSelectedListener true
+                }
+                else -> false
+            }
+        }
     }
 
+    private fun showLogoutConfirmationDialog() {
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        alertDialogBuilder.setTitle("로그아웃 확인")
+        alertDialogBuilder.setMessage("로그아웃 하시겠습니까?")
+
+        alertDialogBuilder.setPositiveButton("Yes") { _, _ ->
+            logoutUser()
+        }
+
+        alertDialogBuilder.setNegativeButton("No") { _, _ ->
+            // 다이얼로그를 닫거나 추가 작업 수행
+        }
+
+        val alertDialog: AlertDialog = alertDialogBuilder.create()
+        alertDialog.show()
+    }
+
+    private fun logoutUser() {
+        FirebaseAuth.getInstance().signOut()
+        navigateToLoginActivity()
+    }
+
+    private fun navigateToLoginActivity() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
 
     private fun loadDiary(date: String) {
-        val scheduleRef = database.child("schedules").child(userID)
-
-        scheduleRef.addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                val scheduleList = mutableListOf<ScheduleItem>()
-
-                for (scheduleSnapshot in dataSnapshot.children) {
-                    val scheduleDate = scheduleSnapshot.child("date").value as? String ?: ""
-                    if (scheduleDate == date) {
-                        val schedule = scheduleSnapshot.child("schedule").value as? String ?: ""
-                        val startTime = scheduleSnapshot.child("startTime").value as? String ?: ""
-                        val endTime = scheduleSnapshot.child("endTime").value as? String ?: ""
-                        val key = scheduleSnapshot.key ?: ""
-
-                        // ScheduleItem을 생성하여 리스트에 추가
-                        val fullScheduleText = "$schedule\n시작 시간: $startTime\n종료 시간: $endTime"
-                        val scheduleItem = ScheduleItem(schedule, startTime, endTime, date,key)
-                        scheduleList.add(scheduleItem)
-                    }
-                }
-
-                // 어댑터에 데이터 설정
-                scheduleAdapter.setData(scheduleList)
-                scheduleAdapter.notifyDataSetChanged()
-
-                // 리스트가 비어있을 때 메시지 표시
-                if (scheduleList.isEmpty()) {
-                    diaryContent.text = "일정이 없습니다."
-                } else {
-                    diaryContent.text = "" // 일정이 있으면 기존 메시지를 지움
-                }
-            }
-
-            override fun onCancelled(databaseError: DatabaseError) {
-                showToast("일정 불러오기 실패: ${databaseError.message}")
-            }
-
-        })
+        // 일정 불러오는 코드
     }
 
     private fun showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
-    private fun navigateToLoginActivity() {
-        // 로그인 액티비티로 이동하는 코드
-    }
-
     private fun navigateToScheduleList() {
         val intent = Intent(this, ScheduleListActivity::class.java)
         intent.putExtra("userID", userID)
         startActivity(intent)
-
     }
+
     private fun navigateToScheduleInputActivity() {
         val intent = Intent(this, ScheduleInputActivity::class.java)
         intent.putExtra("selectedDate", selectedDate)
         startActivity(intent)
-
     }
-
 }
